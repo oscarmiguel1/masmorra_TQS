af0720939b377eeb552d66d3750bf75eae5041f8
oscarmiguel1 <1566337@uab.cat>
2025-10-02 16:15:56 +0200

    commit inicial

---
+++ src/test/java/es/uab/TQS/mazmorra/JugadorTest.java
@@ -0,0 +1,11 @@
+package es.uab.TQS.mazmorra;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JugadorTest {
+    
+
+    public void firstTest() {
+        assertTrue(false);
+    }
+}

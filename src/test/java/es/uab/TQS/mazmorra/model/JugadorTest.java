df0d7750eec3f46504289177acc5ab21bf0c151e
oscarmiguel1 <1566337@uab.cat>
2025-10-06 22:07:08 +0200

    JugadorTest

--- src/test/java/es/uab/TQS/mazmorra/model/JugadorTest.java
+++ src/test/java/es/uab/TQS/mazmorra/model/JugadorTest.java
@@ -1,11 +1,22 @@
 package es.uab.TQS.mazmorra.model;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 public class JugadorTest {
     
 
-    public void testFirst() {
-        assertTrue(false);
+    @Test
+    void testsetHP() {
+       Jugador j = new Jugador();
+
+       j.setHP(-3);
+       assertTrue(j.getHP()==0);
+
+       j.setHP(12);
+       assertTrue(j.getHP() == 12);
+
+       j.setHP(44);
+       assertTrue(j.getHP() == 20);
     }
 }

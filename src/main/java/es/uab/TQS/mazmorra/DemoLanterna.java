000faf7e0f9880a1f681b8ab1df8c87fd51f08c1
oscarmiguel1 <1566337@uab.cat>
2025-10-16 15:40:26 +0200

    Lanterna añadido

---
+++ src/main/java/es/uab/TQS/mazmorra/DemoLanterna.java
@@ -0,0 +1,58 @@
+package es.uab.TQS.mazmorra;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+public class DemoLanterna {
+    public static void main(String[] args) {
+        try {
+
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory()
+                    .setForceAWTOverSwing(false)  // asegura compatibilidad
+                    .setTerminalEmulatorTitle("Mazmorra")
+                    .setPreferTerminalEmulator(true); // fuerza modo ventana
+
+            Terminal terminal = terminalFactory.createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.startScreen();
+            screen.setCursorPosition(null); // oculta el cursor
+
+            TextGraphics tg = screen.newTextGraphics();
+            int jugadorX = 10, jugadorY = 5;
+            boolean seguir = true;
+
+            while (seguir) {
+                screen.clear();
+
+                tg.setForegroundColor(TextColor.ANSI.WHITE);
+                tg.putString(jugadorX, jugadorY, "@");
+
+                tg.setForegroundColor(TextColor.ANSI.YELLOW);
+                for (int x = 0; x < 30; x++) tg.putString(x, 10, "#");
+
+                screen.refresh();
+
+                KeyStroke key = screen.pollInput();
+                if (key != null) {
+                    switch (key.getKeyType()) {
+                        case ArrowUp -> jugadorY--;
+                        case ArrowDown -> jugadorY++;
+                        case ArrowLeft -> jugadorX--;
+                        case ArrowRight -> jugadorX++;
+                        case Escape, EOF -> seguir = false;
+                    }
+                }
+
+                Thread.sleep(50);
+            }
+
+            screen.stopScreen();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/es/uab/TQS/mazmorra/Main.java
+++
@@ -1,9 +0,0 @@
-package es.uab.TQS.mazmorra;
-
-public class Main{
- 
-    public static void main(String[] args ) {
-        System.out.println("Hola!");
-
-    }
-}

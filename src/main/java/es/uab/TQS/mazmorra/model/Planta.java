a9a6fe1b865f6eea926b7d75d54b48d2b24a77da
oscarmiguel1 <1566337@uab.cat>
2025-10-23 19:43:42 +0200

    Classe Planta

--- src/main/java/es/uab/TQS/mazmorra/model/Planta.java
+++ src/main/java/es/uab/TQS/mazmorra/model/Planta.java
@@ -1,5 +1,30 @@
+//Planta
 package es.uab.TQS.mazmorra.model;
 
 public class Planta {
+
+  private boolean doorOpen;
+
+
+  private String[] floorLayout;
+
+  private int enemiesLeft;
+
+  public Planta(int e, String[] f){
+    this.doorOpen = false;
+    this.enemiesLeft = e;
+    this.floorLayout = f;
+  }
+
+  public void openDoor() {this.doorOpen = true;}
+  public int getEnemiesLeft(){ return this.enemiesLeft;}
+
+  public boolean getDoorState() { return this.doorOpen; }
+  public void enemyDefeated() {
+    if(this.enemiesLeft-1 >= 0) {
+      this.enemiesLeft--;
+    }
+  }
+
 
 }

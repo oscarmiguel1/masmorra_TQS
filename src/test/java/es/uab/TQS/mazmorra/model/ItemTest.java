4502fd56c95d1cdcd5175e674dd4182a6985e179
oscarmiguel1 <1566337@uab.cat>
2025-10-24 20:24:14 +0200

    ItemTest

---
+++ src/test/java/es/uab/TQS/mazmorra/model/ItemTest.java
@@ -0,0 +1,15 @@
+package es.uab.TQS.mazmorra.model;
+
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.Test;
+
+public class ItemTest {
+
+  @Test
+  public void usarTest(){
+      Jugador mockJugador = mock(Jugador.class);
+
+
+  }
+}
